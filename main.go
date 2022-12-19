dbd4b2fe7e70db90ae414236f93db1e9196916e8
Adrià Freixas Grimalt <adria.freixas@webbeds.com>
2022-12-19 16:39:52 +0100

    slices from arrays and from other slices

--- main.go
+++ main.go
@@ -5,15 +5,28 @@ import "fmt"
 func main() {
 
 	//regular
-	var arr [3]int
-	arr[0] = 1
-	arr[1] = 2
-	arr[2] = 3
-	fmt.Println(arr)
-	fmt.Println(arr[1])
+	arr := [3]int{1, 2, 3}
+	//: means all indexes
+	aSlice := arr[:]
 
-	//implicit
-	otherArr := [3]int{4, 5, 6}
-	fmt.Println(otherArr)
-	fmt.Println(otherArr[1])
+	fmt.Println(arr, aSlice)
+
+	//acts like a pointer
+	arr[1] = 42
+	aSlice[2] = 27
+	fmt.Println(arr, aSlice)
+
+	slice := []int{1, 2, 3}
+	fmt.Println(slice)
+
+	slice = append(slice, 4, 42, 27)
+	fmt.Println(slice)
+
+	//slice from slice form 1 to end
+	s2 := slice[1:]
+	//up to 2 elements
+	s3 := slice[:2]
+	//indexes 1 and 2
+	s4 := slice[1:2]
+	fmt.Println(s2, s3, s4)
 }

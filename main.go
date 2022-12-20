5b9388a61127115c8c5d049715b4a5f071d66aa9
Adrià Freixas Grimalt <adria.freixas@webbeds.com>
2022-12-20 15:09:26 +0100

    for loop up to 5

--- main.go
+++ main.go
@@ -1,20 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	port := 3000
-	_, err := startWebServer(port, 2)
-	fmt.Println(port, err)
-}
-
-func startWebServer(port, numberOfRetries int) (int, error) {
-	fmt.Println("Starting server...")
-	//do important things
-	fmt.Println("Server started on port", port)
-	fmt.Println("Number of retries is", numberOfRetries)
-
-	return port, nil
+	var i int
+	for i < 5 {
+		println(i)
+		i++
+	}
+	//controllers.RegisterControllers()
+	//http.ListenAndServe(":3000", nil)
 }

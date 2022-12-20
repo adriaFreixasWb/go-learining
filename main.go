4ac1d5b1274fb7861fa587ad3869ceacca738c29
Adrià Freixas Grimalt <adria.freixas@webbeds.com>
2022-12-20 08:51:28 +0100

    add coment on multyline init for structs

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	u.LastName = "Dent"
 	fmt.Println(u)
 
+	//the coma at the end on LastName definition is needed to have your closing brance on a different line
 	u2 := user{ID: 1,
 		FirstName: "Arthur",
 		LastName:  "Dent",

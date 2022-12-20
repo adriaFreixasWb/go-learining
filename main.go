e69b05bd55eff8af2399bdab3622852c93cb7c7b
Adrià Freixas Grimalt <adria.freixas@webbeds.com>
2022-12-20 15:33:38 +0100

    loops over a map

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 func main() {
-	slice := []int{1, 2, 3}
-	for i, v := range slice {
-		println(i, v)
+	wellKnownPorts := map[string]int{"http": 80, "https": 443}
+	for k, v := range wellKnownPorts {
+		println(k, v)
 	}
 	//controllers.RegisterControllers()
 	//http.ListenAndServe(":3000", nil)

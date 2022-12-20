3a7e357da3e14de98b93e9886875effe2e09faab
Adrià Freixas Grimalt <adria.freixas@webbeds.com>
2022-12-20 11:22:25 +0100

    creates a constructor function

--- controllers/users.go
+++ controllers/users.go
@@ -1,10 +1,22 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
 
-type userController struct{}
+type userController struct {
+	userIdPattern *regexp.Regexp
+}
 
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//a byte slice can also be defined as a string
 	w.Write([]byte("Hello from UserController!"))
+}
+
+// constructor function
+func newUserController() *userController {
+	return &userController{
+		userIdPattern: regexp.MustCompile(`^/users/(\d+)/`),
+	}
 }
